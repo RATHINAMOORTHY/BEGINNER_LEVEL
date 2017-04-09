a1c0421b58458e4e45ad38b82254a56c48635de6
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-09 13:42:38 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class alphabet
 {
-public static void main(String args[])
-{
-char c=args[0].charAt(0);
-  if(c.isLetter())
+ public static void main(String args[])
+ {
+  char c=args[0].charAt(0);
+  if(Character.isLetter(cf))
     System.out.print("ALPHABET");
   else
     System.out.print("NOT AN ALPHABET");
-}
+ }
 }
