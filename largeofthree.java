4d406d3204238af424bd7c0d0a9766f362bbb44e
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-09 13:54:11 +0530

    Update largeofthree.java

--- largeofthree.java
+++ largeofthree.java
@@ -6,6 +6,6 @@ class largeofthree
     int a=Integer.valueOf(args[0]);
     int b=Integer.valueOf(args[1]);
     int c=Integer.valueOf(args[2]);
-    System.out.print(Math.min(Math.min(a,b),c));
+    System.out.print(Math.max(Math.max(a,b),c));
   }
 }
