f9a16b2f052ceb4f8bcb58a008b7d64bf737b857
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-09 12:59:55 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -5,7 +5,7 @@ class vowel
     System.out.println("ENTER A CHARACTER");
     Scanner in=new Scanner(System.in);
     String c=in.nextLine();
-    if(c.equalsIgnoreCase("a"))
+    if(c.equalsIgnoreCase("a")||c.equalsIgnoreCase("e")||c.equalsIgnoreCase("i")||c.equalsIgnoreCase("o")||c.equalsIgnoreCase("u"))
     System.out.println("VOWEL");
   }
 }
