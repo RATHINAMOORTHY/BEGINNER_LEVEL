dfdcab35f322088bab0410b432247b96c35d9626
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-09 12:56:32 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -1,15 +1,10 @@
+import java.util.*;
 class vowel
 {
-  public static void main(String args[0]))
-  {
-    String s1="a";
-    String s2="e";
-    String s3="i";
-    String s4="o";
-    String s5="u";
-    if(args[0].equalsIgnoreCase(s1)||args[0].equalsIgnoreCase(s2)||args[0].equalsIgnoreCase(s3)||args[0].equalsIgnoreCase(s4)||args[0].equalsIgnoreCase(s5))
-       System.out.print("VOWEL");
-    else
-       System.out.print("NOT VOWEL");
+  public static void main(String args[]) {
+    System.out.println("ENTER A CHARACTER");
+    Scanner in=new Scanner(System.in);
+    String c=in.nextLine();
+    System.out.println(c);
   }
 }
