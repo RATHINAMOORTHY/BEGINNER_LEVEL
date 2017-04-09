473b4fcb10996c103b3b30de6515e5a0a183fd65
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-09 11:15:01 +0530

    Update one.java

--- one.java
+++ one.java
@@ -2,7 +2,12 @@ class one
 {
  public static void main(String args[])
   {
-    int n=Integer.valueOf(args[0]);
-    System.out.print(+n);
+  int n=Integer.valueOf(args[0]);
+  if(n==0)
+    System.out.print("ZERO");
+  else if(n>0)
+   System.out.print("POSITIVE");
+  else
+   System.out.print("NEGATIVE");
   }
 }
