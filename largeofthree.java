d9643153d7904bf7c77c93127ef16c91cb0a499c
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-09 13:52:24 +0530

    Update largeofthree.java

--- largeofthree.java
+++ largeofthree.java
@@ -1,1 +1,11 @@
-
+import java.math.*;
+class largeofthree
+{
+  public static void main(String args[])
+  {
+    int a=Integer.valueOf(args[0]);
+    int b=Integer.valueOf(args[1]);
+    int c=Integer.valueOf(args[2]);
+    System.out.print(Math.min(Math.min(a,b),c));
+  }
+}
