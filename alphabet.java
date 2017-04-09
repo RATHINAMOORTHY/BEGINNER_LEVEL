7d15333347acdfd1d199adddf094f6a26d95b90d
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-09 13:39:11 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -3,3 +3,10 @@ class alphabet
 {
 public static void main(String args[])
 {
+char c=args[0].charAt(0);
+  if(c.isLetter())
+    System.out.print("ALPHABET");
+  else
+    System.out.print("NOT AN ALPHABET");
+}
+}
