399cf68e2e9133b60a142fe8596bb20b70fb368b
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-09 11:25:52 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,11 @@
+class oddeven
+{
+	public static void main(String args[])
+	{
+		int n=Integer.valueOf(args[0]);
+		if(n%2==0)
+			System.out.print("EVEN");
+		else 
+		    System.out.print("ODD");
+	}
+}
