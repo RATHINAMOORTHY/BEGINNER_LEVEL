6ae6dc5791d813e0af9c2f008ff3c2fc031d4756
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-10 07:04:24 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,1 +1,12 @@
-
+import java.util.*;
+class palindrome
+{
+  public static void main(String args[])
+  {
+    StringBuffer sb=new StringBuffer(args[0]);
+    if(sb.reverse()==args[0])
+      System.out.print("PALINDROME");
+    else
+      System.out.print("NOT PALINDROME");
+  }
+}
