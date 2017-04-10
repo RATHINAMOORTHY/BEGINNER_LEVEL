8a48c9796a9fe2701ce098809dabadb438ec5950
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-10 18:18:26 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,1 +1,24 @@
-
+class prime
+{
+  public static void main(String args[])
+  {
+    int flag=0;
+    int a=Integer.valueOf(args[0]);
+    int b=a/2;
+    for(int i=0;i<b;i++)
+    {
+      if(a%i==0)
+      {
+        flag=1;
+        System.out.print("NOT PRIME");
+        break;
+      }
+    }
+    if(flag==0)
+    {
+       System.out.print("PRIME");
+    }
+  }
+}
+      
+      
