6f4337bfadbbd9090dcc5686b5f6c3960b34522f
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-10 18:05:50 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -4,7 +4,10 @@ class palindrome
   public static void main(String args[])
   {
     StringBuffer sb=new StringBuffer(args[0]);
-    if(sb.reverse()==args[0])
+    sb.reverse();
+    String s=new String(sb);
+    
+    if(s.toUpperCase()==args[0].toUpperCase())
       System.out.print("PALINDROME");
     else
       System.out.print("NOT PALINDROME");
