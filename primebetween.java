c1b27beea466078eb3eab83dab844808cbd690b8
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-10 18:38:27 +0530

    Update primebetween.java

--- primebetween.java
+++ primebetween.java
@@ -1,1 +1,21 @@
-
+class primebetween
+{
+  public static void main(String args[])
+  {
+    int a=Integer.valueOf(args[0]);
+    int b=Integer.valueOf(args[1]);
+    for(int i=a;i<=b;i++)
+    {
+      int n=i;
+      int m=n/2;
+      for(int j=2;j<=m;j++)
+      {
+        if(n%j!=0)
+        {
+          System..out.println(+j);
+        }
+      }
+    }
+  }
+}
+          
