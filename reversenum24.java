cdd17938b5027bbff8179639bfafc6371a614139
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-19 07:23:06 +0530

    Update reversenum24.java

--- reversenum24.java
+++ reversenum24.java
@@ -3,6 +3,6 @@ class reversenum24
   public static void main(String args[])
   {
     StringBuffer sb=new StringBuffer(args[0]);
-    system.out.print(sb.reverse());
+    System.out.print(sb.reverse());
   }
 }
