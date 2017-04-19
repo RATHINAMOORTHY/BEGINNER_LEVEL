26c4503925872a113033b9733c735159cf27c271
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-19 07:28:03 +0530

    Update asci.java

--- asci.java
+++ asci.java
@@ -1,1 +1,11 @@
-
+class asci
+{
+  public static void main(String args[])
+  {
+    char[] c=args[0].toCharArray();
+    for(int i=0;i<args[0].length;i++)
+    {
+      System.out.print(+(int)c[i]);
+    }
+  }
+}
