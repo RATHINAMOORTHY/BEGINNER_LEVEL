3c2886dd8361350efa1fcbf9db5e36af16f3b811
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-05-18 08:00:13 +0530

    Update power11.java

--- power11.java
+++ power11.java
@@ -1,1 +1,8 @@
-
+import java.math.*;
+class power11
+{
+  public static void main(String args[])
+  {
+    int a=Integer.valueOf(args[0]);
+    int b=Integer.valueOf(args[1]);
+    System.out.print(+Math.pow
