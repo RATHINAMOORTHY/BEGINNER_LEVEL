70c82d5cc932aaff7aa9d9258f7de31a28ddb4ba
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-05-22 07:03:33 +0530

    Update semicolon41.java

--- semicolon41.java
+++ semicolon41.java
@@ -2,8 +2,7 @@ class semicolon41
 {
   public static void main(String args[])
   {
-    int i;
-    for(int i=0;i<2;System.out.print("Hello world"))
+    for(int i=0;i<1;System.out.print("Hello world"))
     {
       i++;
     }
