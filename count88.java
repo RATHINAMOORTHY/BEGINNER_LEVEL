6ba6705dab703292de007ca964c4808bef902b43
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-04 00:01:49 +0530

    Update count88.java

--- count88.java
+++ count88.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+public class count88 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+    	System.out.println("ENTER THE STRING");
+   		String s=in.nextLine();
+   		char[] c=s.toCharArray();
+   		int sum=0,i;
+   		for(i=0;i<s.length();i++)
+   		{
+   			if(Character.isLetter(c[i]))
+   			{
+   				sum++;
+   			}
+   		}
+   		
+   		
+   			System.out.print(+sum);
+
+	}
+
+}
 
